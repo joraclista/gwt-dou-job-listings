fc912b5df94f4174e598f0df3664142a8cedef33
joraclista <31856684+joraclista@users.noreply.github.com>
2018-02-28 17:28:53 +0200

    added redirect to dou.ua on vacancy click

--- src/main/java/com/github/joraclista/douJobListingsApplication/client/ui/VacancyBlock.java
+++ src/main/java/com/github/joraclista/douJobListingsApplication/client/ui/VacancyBlock.java
@@ -6,7 +6,10 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.Label;
+
+import static com.google.gwt.user.client.Window.Location.assign;
 
 /**
  * Created by Alisa
@@ -25,9 +28,13 @@ public class VacancyBlock extends Composite {
     Label cities;
     @UiField
     Label info;
+    @UiField
+    FocusPanel container;
 
     public VacancyBlock(JobVacancy vacancy) {
         initWidget(ourUiBinder.createAndBindUi(this));
+
+        container.addClickHandler(event -> assign(vacancy.getUrl()));
         title.setText(vacancy.getPosition());
         company.setText(vacancy.getCompany());
         cities.setText(vacancy.getCities());
